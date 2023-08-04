858c0142a7d781628cc5b6c33ce9b2ca163a09ea
Conless <conlesspan@outlook.com>
2023-08-04 16:40:22 +0800

    docs: update the road map.

--- docs/roadmap.md
+++ docs/roadmap.md
@@ -2,7 +2,13 @@
 ## Semantic
 - [x] 07-27 ```.g4``` files for Mx* grammar
 - [x] 07-30 Build AST from CST
-- [ ] 07-31 Symbol collector
-- [ ] 08-02 Semantic Check
+- [x] 07-31 Symbol collector (08-01)
+- [x] 08-02 Semantic Check (08-03)
+- [ ] 08-04 Refactor code
+## LLVM IR
+- [ ] 08-05 Finish design of IR structure
+- [ ] 08-08 Main part of IR
+- [ ] 08-10 Pass IR
+
 ## Code Generation
 To be determined.
---
+++ src/main/java/dev/conless/comet/frontend/ir/IRBuilder.java
@@ -0,0 +1,178 @@
+package dev.conless.comet.frontend.ir;
+
+import dev.conless.comet.frontend.ast.*;
+import dev.conless.comet.frontend.ast.def.*;
+import dev.conless.comet.frontend.ast.expr.*;
+import dev.conless.comet.frontend.ast.stmt.*;
+import dev.conless.comet.frontend.ast.type.*;
+
+public class IRBuilder implements ASTVisitor {
+  @Override
+  public void visit(ASTNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(ProgramNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(FuncDefNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(ClassDefNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(VarDefNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(TypeNameNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(ExprNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(NewExprNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(MemberExprNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(CallExprNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(IndexExprNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(PostUnaryExprNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(PreUnaryExprNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(BinaryExprNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(ConditionalExprNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(AssignExprNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(AtomExprNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(StmtNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(BlockStmtNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(IfStmtNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(ForStmtNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(WhileStmtNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(ContinueStmtNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(BreakStmtNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(ReturnStmtNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(ExprStmtNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(VarDefStmtNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+
+  @Override
+  public void visit(EmptyStmtNode node) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visit'");
+  }
+  
+}
