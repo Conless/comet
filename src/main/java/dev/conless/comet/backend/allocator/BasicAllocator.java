774878ac289f94acd17c4a205e83e448fbc34ac9
Conless <conlesspan@outlook.com>
2023-09-24 16:18:01 +0800

    refactor(backend/allocator): renaming code shared.

--- src/main/java/dev/conless/comet/backend/allocator/BasicAllocator.java
+++ src/main/java/dev/conless/comet/backend/allocator/BasicAllocator.java
@@ -1,5 +1,6 @@
 package dev.conless.comet.backend.allocator;
 
+import dev.conless.comet.backend.asm.ASMVisitor;
 import dev.conless.comet.backend.asm.entity.ASMAddress;
 import dev.conless.comet.backend.asm.entity.ASMPhysicalReg;
 import dev.conless.comet.backend.asm.entity.ASMReg;
@@ -15,9 +16,8 @@ import dev.conless.comet.utils.container.Array;
 import dev.conless.comet.utils.container.Set;
 import dev.conless.comet.utils.error.*;
 
-public class BasicAllocator extends ASMManager implements RegAllocator {
+public class BasicAllocator extends RegAllocator {
   private Set<ASMPhysicalReg> cleanRegs;
-  private ASMFuncDefNode currentFunc;
 
   public BasicAllocator() {
     cleanRegs = new Set<>();
@@ -53,159 +53,6 @@ public class BasicAllocator extends ASMManager implements RegAllocator {
   }
 
   @Override
-  public ASMNode visit(ASMBlockStmtNode node) {
-    var stmts = new ASMStmtNode();
-    for (var inst : node.getNodes()) {
-      var newStmt = inst.accept(this);
-      if (newStmt instanceof ASMStmtNode newStmts) {
-        stmts.appendNodes(newStmts);
-      } else {
-        stmts.addNode((ASMInstNode) newStmt);
-      }
-    }
-    node.setNodes(stmts.getNodes());
-    var exitInst = new ASMStmtNode();
-    for (var inst : node.getExitInst().getNodes()) {
-      var newStmt = inst.accept(this);
-      if (newStmt instanceof ASMStmtNode newStmts) {
-        exitInst.appendNodes(newStmts);
-      } else {
-        exitInst.addNode((ASMInstNode) newStmt);
-      }
-    }
-    node.setExitInst(exitInst);
-    return node;
-  }
-
-  @Override
-  public ASMNode visit(ASMStrDefNode node) {
-    throw new RuntimeError("BasicAllocator.visit(ASMStrDefNode) should not be called");
-  }
-
-  @Override
-  public ASMNode visit(ASMVarDefNode node) {
-    throw new RuntimeError("BasicAllocator.visit(ASMVarDefNode) should not be called");
-  }
-
-  @Override
-  public ASMNode visit(ASMBeqzNode node) {
-    var instList = new ASMStmtNode();
-    var reg = getRValueReg(node.getEntity(), instList);
-    node.setEntity(reg);
-    instList.addNode(node);
-    evictReg(instList, reg);
-    return instList;
-  }
-
-  @Override
-  public ASMNode visit(ASMBinaryNode node) {
-    var instList = new ASMStmtNode();
-    var lhs = getRValueReg(node.getLhs(), instList);
-    var rhs = getRValueReg(node.getRhs(), instList);
-    var dest = getLValueReg(node.getDest());
-    node.setLhs(lhs);
-    node.setRhs(rhs);
-    node.setDest(dest);
-    instList.addNode(node);
-    evictReg(instList, lhs, rhs, dest);
-    return instList;
-  }
-
-  @Override
-  public ASMNode visit(ASMCallNode node) {
-    currentFunc.getUsedRegs().add(regs.getRa());
-    return node;
-  }
-
-  @Override
-  public ASMNode visit(ASMJumpNode node) {
-    return node;
-  }
-
-  @Override
-  public ASMNode visit(ASMLoadAddrNode node) {
-    var instList = new ASMStmtNode();
-    var dest = getLValueReg(node.getDest());
-    node.setDest(dest);
-    instList.addNode(node);
-    evictReg(instList, dest);
-    return instList;
-  }
-
-  @Override
-  public ASMNode visit(ASMLoadImmNode node) {
-    var instList = new ASMStmtNode();
-    var dest = getLValueReg(node.getDest());
-    node.setDest(dest);
-    instList.addNode(node);
-    evictReg(instList, dest);
-    return instList;
-  }
-
-  @Override
-  public ASMNode visit(ASMLoadNode node) {
-    var instList = new ASMStmtNode();
-    var dest = getLValueReg(node.getDest());
-    var addr = getRValueReg(node.getSrc().getBase(), instList);
-    node.setDest(dest);
-    node.setSrc(new ASMAddress(addr, node.getSrc().getOffset()));
-    instList.addNode(node);
-    evictReg(instList, dest, addr);
-    return instList;
-  }
-
-  @Override
-  public ASMNode visit(ASMMoveNode node) {
-    var instList = new ASMStmtNode();
-    var src = getRValueReg(node.getSrc(), instList);
-    var dest = getLValueReg(node.getDest());
-    node.setSrc(src);
-    node.setDest(dest);
-    instList.addNode(node);
-    evictReg(instList, src, dest);
-    return instList;
-  }
-
-  @Override
-  public ASMNode visit(ASMReturnNode node) {
-    return node;
-  }
-
-  @Override
-  public ASMNode visit(ASMStoreNode node) {
-    var instList = new ASMStmtNode();
-    var src = getRValueReg(node.getSrc(), instList);
-    var dest = getRValueReg(node.getDest().getBase(), instList);
-    node.setSrc(src);
-    node.setDest(new ASMAddress(dest, node.getDest().getOffset()));
-    instList.addNode(node);
-    evictReg(instList, src, dest);
-    return instList;
-  }
-
-  @Override
-  public ASMNode visit(ASMUnaryNode node) {
-    var instList = new ASMStmtNode();
-    var src = getRValueReg(node.getOperand(), instList);
-    var dest = getLValueReg(node.getDest());
-    node.setOperand(src);
-    node.setDest(dest);
-    instList.addNode(node);
-    evictReg(instList, src, dest);
-    return instList;
-  }
-
-  @Override
-  public ASMNode visit(ASMCommentNode node) {
-    return node;
-  }
-
-  @Override
-  public ASMNode visit(ASMLabelNode node) {
-    return node;
-  }
-
-  @Override
   public ASMPhysicalReg getRValueReg(ASMReg reg, ASMStmtNode nodes) {
     if (reg instanceof ASMPhysicalReg) {
       return (ASMPhysicalReg) reg;
@@ -232,14 +79,14 @@ public class BasicAllocator extends ASMManager implements RegAllocator {
     newReg.setDirty(true);
     newReg.setVirtualID(((ASMVirtualReg) reg).getID());
     currentFunc.getUsedRegs().add(newReg);
-    currentFunc.setMemUsed(Math.max(currentFunc.getMemUsed(), ((ASMVirtualReg) reg).getID() + 1));
+    currentFunc.setSpilled(Math.max(currentFunc.getSpilled(), ((ASMVirtualReg) reg).getID() + 1));
     return newReg;
   }
 
   @Override
   public void evictReg(ASMStmtNode nodes, ASMPhysicalReg... dirtyRegs) {
     for (var reg : dirtyRegs) {
-      if (!reg.getName().startsWith("t")) {
+      if (!regs.getTempRegs().contains(reg)) {
         continue;
       }
       if (reg.isDirty()) {
--- src/main/java/dev/conless/comet/backend/allocator/RegAllocator.java
+++ src/main/java/dev/conless/comet/backend/allocator/RegAllocator.java
@@ -1,13 +1,188 @@
 package dev.conless.comet.backend.allocator;
 
 import dev.conless.comet.backend.asm.ASMVisitor;
+import dev.conless.comet.backend.asm.entity.ASMAddress;
 import dev.conless.comet.backend.asm.entity.ASMPhysicalReg;
 import dev.conless.comet.backend.asm.entity.ASMReg;
 import dev.conless.comet.backend.asm.node.ASMNode;
+import dev.conless.comet.backend.asm.node.global.ASMStrDefNode;
+import dev.conless.comet.backend.asm.node.global.ASMVarDefNode;
+import dev.conless.comet.backend.asm.node.inst.ASMBeqzNode;
+import dev.conless.comet.backend.asm.node.inst.ASMBinaryNode;
+import dev.conless.comet.backend.asm.node.inst.ASMCallNode;
+import dev.conless.comet.backend.asm.node.inst.ASMInstNode;
+import dev.conless.comet.backend.asm.node.inst.ASMJumpNode;
+import dev.conless.comet.backend.asm.node.inst.ASMLoadAddrNode;
+import dev.conless.comet.backend.asm.node.inst.ASMLoadImmNode;
+import dev.conless.comet.backend.asm.node.inst.ASMLoadNode;
+import dev.conless.comet.backend.asm.node.inst.ASMMoveNode;
+import dev.conless.comet.backend.asm.node.inst.ASMReturnNode;
+import dev.conless.comet.backend.asm.node.inst.ASMStoreNode;
+import dev.conless.comet.backend.asm.node.inst.ASMUnaryNode;
+import dev.conless.comet.backend.asm.node.stmt.ASMBlockStmtNode;
 import dev.conless.comet.backend.asm.node.stmt.ASMStmtNode;
+import dev.conless.comet.backend.asm.node.utils.ASMCommentNode;
+import dev.conless.comet.backend.asm.node.utils.ASMLabelNode;
+import dev.conless.comet.backend.asm.utils.ASMManager;
+import dev.conless.comet.utils.error.RuntimeError;
 
-public interface RegAllocator extends ASMVisitor<ASMNode> {
-  public ASMPhysicalReg getRValueReg(ASMReg reg, ASMStmtNode nodes);
-  public ASMPhysicalReg getLValueReg(ASMReg reg);
-  public void evictReg(ASMStmtNode nodes, ASMPhysicalReg... dirtyRegs);
+public abstract class RegAllocator extends ASMManager implements ASMVisitor<ASMNode> {
+  @Override
+  public ASMNode visit(ASMBlockStmtNode node) {
+    var stmts = new ASMStmtNode();
+    for (var inst : node.getNodes()) {
+      var newStmt = inst.accept(this);
+      if (newStmt instanceof ASMStmtNode newStmts) {
+        stmts.appendNodes(newStmts);
+      } else {
+        stmts.addNode((ASMInstNode) newStmt);
+      }
+    }
+    node.setNodes(stmts.getNodes());
+    var exitInst = new ASMStmtNode();
+    for (var inst : node.getExitInst().getNodes()) {
+      var newStmt = inst.accept(this);
+      if (newStmt instanceof ASMStmtNode newStmts) {
+        exitInst.appendNodes(newStmts);
+      } else {
+        exitInst.addNode((ASMInstNode) newStmt);
+      }
+    }
+    node.setExitInst(exitInst);
+    return node;
+  }
+
+  @Override
+  public ASMNode visit(ASMStrDefNode node) {
+    throw new RuntimeError("BasicAllocator.visit(ASMStrDefNode) should not be called");
+  }
+
+  @Override
+  public ASMNode visit(ASMVarDefNode node) {
+    throw new RuntimeError("BasicAllocator.visit(ASMVarDefNode) should not be called");
+  }
+
+  @Override
+  public ASMNode visit(ASMBeqzNode node) {
+    var instList = new ASMStmtNode();
+    var reg = getRValueReg(node.getEntity(), instList);
+    node.setEntity(reg);
+    instList.addNode(node);
+    evictReg(instList, reg);
+    return instList;
+  }
+
+  @Override
+  public ASMNode visit(ASMBinaryNode node) {
+    var instList = new ASMStmtNode();
+    var lhs = getRValueReg(node.getLhs(), instList);
+    var rhs = getRValueReg(node.getRhs(), instList);
+    var dest = getLValueReg(node.getDest());
+    node.setLhs(lhs);
+    node.setRhs(rhs);
+    node.setDest(dest);
+    instList.addNode(node);
+    evictReg(instList, lhs, rhs, dest);
+    return instList;
+  }
+
+  @Override
+  public ASMNode visit(ASMCallNode node) {
+    currentFunc.getUsedRegs().add(regs.getRa());
+    return node;
+  }
+
+  @Override
+  public ASMNode visit(ASMJumpNode node) {
+    return node;
+  }
+
+  @Override
+  public ASMNode visit(ASMLoadAddrNode node) {
+    var instList = new ASMStmtNode();
+    var dest = getLValueReg(node.getDest());
+    node.setDest(dest);
+    instList.addNode(node);
+    evictReg(instList, dest);
+    return instList;
+  }
+
+  @Override
+  public ASMNode visit(ASMLoadImmNode node) {
+    var instList = new ASMStmtNode();
+    var dest = getLValueReg(node.getDest());
+    node.setDest(dest);
+    instList.addNode(node);
+    evictReg(instList, dest);
+    return instList;
+  }
+
+  @Override
+  public ASMNode visit(ASMLoadNode node) {
+    var instList = new ASMStmtNode();
+    var dest = getLValueReg(node.getDest());
+    var addr = getRValueReg(node.getSrc().getBase(), instList);
+    node.setDest(dest);
+    node.setSrc(new ASMAddress(addr, node.getSrc().getOffset()));
+    instList.addNode(node);
+    evictReg(instList, dest, addr);
+    return instList;
+  }
+
+  @Override
+  public ASMNode visit(ASMMoveNode node) {
+    var instList = new ASMStmtNode();
+    var src = getRValueReg(node.getSrc(), instList);
+    var dest = getLValueReg(node.getDest());
+    node.setSrc(src);
+    node.setDest(dest);
+    instList.addNode(node);
+    evictReg(instList, src, dest);
+    return instList;
+  }
+
+  @Override
+  public ASMNode visit(ASMReturnNode node) {
+    return node;
+  }
+
+  @Override
+  public ASMNode visit(ASMStoreNode node) {
+    var instList = new ASMStmtNode();
+    var src = getRValueReg(node.getSrc(), instList);
+    var dest = getRValueReg(node.getDest().getBase(), instList);
+    node.setSrc(src);
+    node.setDest(new ASMAddress(dest, node.getDest().getOffset()));
+    instList.addNode(node);
+    evictReg(instList, src, dest);
+    return instList;
+  }
+
+  @Override
+  public ASMNode visit(ASMUnaryNode node) {
+    var instList = new ASMStmtNode();
+    var src = getRValueReg(node.getOperand(), instList);
+    var dest = getLValueReg(node.getDest());
+    node.setOperand(src);
+    node.setDest(dest);
+    instList.addNode(node);
+    evictReg(instList, src, dest);
+    return instList;
+  }
+
+  @Override
+  public ASMNode visit(ASMCommentNode node) {
+    return node;
+  }
+
+  @Override
+  public ASMNode visit(ASMLabelNode node) {
+    return node;
+  }
+
+  public abstract ASMPhysicalReg getRValueReg(ASMReg reg, ASMStmtNode nodes);
+
+  public abstract ASMPhysicalReg getLValueReg(ASMReg reg);
+  
+  public abstract void evictReg(ASMStmtNode nodes, ASMPhysicalReg... dirtyRegs);
 }
